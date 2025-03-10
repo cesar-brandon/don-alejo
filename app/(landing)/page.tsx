8f7997fe2cda7ab6cf5860b0b2b4eb42e8fbdefd
Cesar Brandon <cesarbrandon000@gmail.com>
2025-03-10 12:15:34 -0500

    First page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,107 +1,101 @@
-import Image from "next/image";
-import { MapPin, Phone, Mail, Clock, Facebook, Instagram } from "lucide-react";
 import HeroSection from "@/modules/landing/components/sections/hero-section";
-import MenuSection from "@/modules/landing/components/sections/menu-section";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-secondary">
       <HeroSection />
 
-      {/* About Section */}
-      <section className="py-16">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-col md:flex-row items-center gap-8">
-            <div className="md:w-1/2">
-              <Image
-                src="/images/presentation/2.jpg"
-                alt="Fundadores del restaurante"
-                width={500}
-                height={400}
-                className="rounded-lg shadow-md"
-              />
-            </div>
-            <div className="md:w-1/2">
-              <h2 className="text-3xl font-semibold mb-4">Nuestra Historia</h2>
-              <p className="text-lg mb-4">
-                Fundado por los abuelos María y José en 1975, Sabores de Abuela
-                ha sido el corazón de la cocina Chinchana en nuestra comunidad
-                por décadas. Cada plato cuenta una historia de tradición, amor y
-                los sabores únicos de nuestra tierra.
-              </p>
-              <p className="text-lg">
-                Hoy, continuamos su legado, sirviendo los mismos platos caseros
-                que han deleitado a generaciones, en un ambiente que te hace
-                sentir como en casa de la abuela.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
+      {/* <section className="py-16"> */}
+      {/*   <div className="container mx-auto px-4"> */}
+      {/*     <div className="flex flex-col md:flex-row items-center gap-8"> */}
+      {/*       <div className="md:w-1/2"> */}
+      {/*         <Image */}
+      {/*           src="/images/presentation/2.jpg" */}
+      {/*           alt="Fundadores del restaurante" */}
+      {/*           width={500} */}
+      {/*           height={400} */}
+      {/*           className="rounded-lg shadow-md" */}
+      {/*         /> */}
+      {/*       </div> */}
+      {/*       <div className="md:w-1/2"> */}
+      {/*         <h2 className="text-3xl font-semibold mb-4">Nuestra Historia</h2> */}
+      {/*         <p className="text-lg mb-4"> */}
+      {/*           Fundado por los abuelos María y José en 1975, Sabores de Abuela */}
+      {/*           ha sido el corazón de la cocina Chinchana en nuestra comunidad */}
+      {/*           por décadas. Cada plato cuenta una historia de tradición, amor y */}
+      {/*           los sabores únicos de nuestra tierra. */}
+      {/*         </p> */}
+      {/*         <p className="text-lg"> */}
+      {/*           Hoy, continuamos su legado, sirviendo los mismos platos caseros */}
+      {/*           que han deleitado a generaciones, en un ambiente que te hace */}
+      {/*           sentir como en casa de la abuela. */}
+      {/*         </p> */}
+      {/*       </div> */}
+      {/*     </div> */}
+      {/*   </div> */}
+      {/* </section> */}
 
-      {/* Menu Section */}
-      <MenuSection />
+      {/* <MenuSection /> */}
 
-      {/* Location and Contact Section */}
-      <section className="py-16 bg-background">
-        <div className="container mx-auto px-4">
-          <div className="grid md:grid-cols-2 gap-8">
-            <div>
-              <h3 className="text-2xl font-semibold mb-4">Encuéntranos</h3>
-              <div className="aspect-video relative rounded-lg overflow-hidden shadow-md"></div>
-              <div className="mt-4 flex items-start">
-                <MapPin className="h-5 w-5   mt-1 mr-2 flex-shrink-0" />
-                <p>Av. Benavides 123, Chincha Alta, Ica, Perú</p>
-              </div>
-            </div>
-            <div>
-              <h3 className="text-2xl font-semibold mb-4">Contáctanos</h3>
-              <div className="space-y-4">
-                <div className="flex items-center">
-                  <Phone className="h-5 w-5 text-secondary mr-2" />
-                  <p>(056) 123-4567</p>
-                </div>
-                <div className="flex items-center">
-                  <Mail className="h-5 w-5 text-secondary mr-2" />
-                  <p>info@saboresdeabuela.com</p>
-                </div>
-                <div className="flex items-start">
-                  <Clock className="h-5 w-5 text-secondary mt-1 mr-2 flex-shrink-0" />
-                  <div>
-                    <p className="font-semibold">Horario de Atención:</p>
-                    <p>Martes a Domingo: 11:00 AM - 10:00 PM</p>
-                    <p>Lunes: Cerrado</p>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-6">
-                <h4 className="text-xl font-semibold mb-2">Síguenos</h4>
-                <div className="flex space-x-4">
-                  <a
-                    href="#"
-                    className="text-secondary hover:text-secondary-700"
-                  >
-                    <Facebook className="h-6 w-6" />
-                  </a>
-                  <a
-                    href="#"
-                    className="text-secondary hover:text-secondary-700"
-                  >
-                    <Instagram className="h-6 w-6" />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      {/* <section className="py-16 bg-background"> */}
+      {/*   <div className="container mx-auto px-4"> */}
+      {/*     <div className="grid md:grid-cols-2 gap-8"> */}
+      {/*       <div> */}
+      {/*         <h3 className="text-2xl font-semibold mb-4">Encuéntranos</h3> */}
+      {/*         <div className="aspect-video relative rounded-lg overflow-hidden shadow-md"></div> */}
+      {/*         <div className="mt-4 flex items-start"> */}
+      {/*           <MapPin className="h-5 w-5   mt-1 mr-2 flex-shrink-0" /> */}
+      {/*           <p>Av. Benavides 123, Chincha Alta, Ica, Perú</p> */}
+      {/*         </div> */}
+      {/*       </div> */}
+      {/*       <div> */}
+      {/*         <h3 className="text-2xl font-semibold mb-4">Contáctanos</h3> */}
+      {/*         <div className="space-y-4"> */}
+      {/*           <div className="flex items-center"> */}
+      {/*             <Phone className="h-5 w-5 text-secondary mr-2" /> */}
+      {/*             <p>(056) 123-4567</p> */}
+      {/*           </div> */}
+      {/*           <div className="flex items-center"> */}
+      {/*             <Mail className="h-5 w-5 text-secondary mr-2" /> */}
+      {/*             <p>info@saboresdeabuela.com</p> */}
+      {/*           </div> */}
+      {/*           <div className="flex items-start"> */}
+      {/*             <Clock className="h-5 w-5 text-secondary mt-1 mr-2 flex-shrink-0" /> */}
+      {/*             <div> */}
+      {/*               <p className="font-semibold">Horario de Atención:</p> */}
+      {/*               <p>Martes a Domingo: 11:00 AM - 10:00 PM</p> */}
+      {/*               <p>Lunes: Cerrado</p> */}
+      {/*             </div> */}
+      {/*           </div> */}
+      {/*         </div> */}
+      {/*         <div className="mt-6"> */}
+      {/*           <h4 className="text-xl font-semibold mb-2">Síguenos</h4> */}
+      {/*           <div className="flex space-x-4"> */}
+      {/*             <a */}
+      {/*               href="#" */}
+      {/*               className="text-secondary hover:text-secondary-700" */}
+      {/*             > */}
+      {/*               <Facebook className="h-6 w-6" /> */}
+      {/*             </a> */}
+      {/*             <a */}
+      {/*               href="#" */}
+      {/*               className="text-secondary hover:text-secondary-700" */}
+      {/*             > */}
+      {/*               <Instagram className="h-6 w-6" /> */}
+      {/*             </a> */}
+      {/*           </div> */}
+      {/*         </div> */}
+      {/*       </div> */}
+      {/*     </div> */}
+      {/*   </div> */}
+      {/* </section> */}
 
       {/* Footer */}
-      <footer className="bg-secondary text-secondary-foreground py-6">
+      <footer className="absolute bottom-0 text-foreground py-6">
         <div className="container mx-auto px-4 text-center">
           <p>
-            &copy; {new Date().getFullYear()} Sabores de Abuela. Todos los
-            derechos reservados.
+            &copy; {new Date().getFullYear()} Don Alejo. Todos los derechos
+            reservados.
           </p>
         </div>
       </footer>
--- modules/landing/components/sections/hero-section.tsx
+++ modules/landing/components/sections/hero-section.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HeroSection() {
   return (
-    <section className="relative h-screen flex items-center justify-center overflow-hidden">
-      {/* Background Image */}
+    <section className="relative h-dvh flex items-center justify-center overflow-hidden">
       <Image
         src="/hero-background.jpg"
         alt="Fondo de cocina chinchana"
@@ -12,11 +12,8 @@ export default function HeroSection() {
         quality={100}
         priority
       />
+      <div className="absolute inset-0 bg-background bg-opacity-50" />
 
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-50" />
-
-      {/* Content */}
       <div className="relative z-10 text-center text-white px-4">
         <Image
           src="/images/logo.jpg"
@@ -25,15 +22,17 @@ export default function HeroSection() {
           height={200}
           className="mx-auto mb-8 rounded-full border-4 border-amber-200"
         />
-        <h1 className="text-5xl md:text-6xl font-bold mb-4  ">
-          Sabores de Abuela
-        </h1>
-        <p className="text-xl md:text-2xl mb-8 max-w-2xl mx-auto">
-          Donde los sabores de Chincha se mezclan con el amor de la abuela
-        </p>
-        <button className="bg-secondary text-secondary-foreground font-semibold py-3 px-8 rounded-full text-lg hover:bg-secondary/80 transition-colors duration-300 shadow-lg">
-          Reserva Ahora
-        </button>
+        {/* <h1 className="text-5xl md:text-6xl font-bold mb-4  "> */}
+        {/*   Sabores de Abuela */}
+        {/* </h1> */}
+        {/* <p className="text-xl md:text-2xl mb-8 max-w-2xl mx-auto"> */}
+        {/*   Donde los sabores de Chincha se mezclan con el amor de la abuela */}
+        {/* </p> */}
+        <Link href="/login">
+          <button className="bg-secondary text-secondary-foreground font-semibold py-3 px-8 rounded-full text-lg hover:bg-secondary/80 transition-colors duration-300 shadow-lg">
+            Iniciar Sesión
+          </button>
+        </Link>
       </div>
     </section>
   );
